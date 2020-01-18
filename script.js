f5268af2929e70dd3c2a5abe66c9e26ca48c80bf
leoniesenevi <leoniesenevi@yahoo.com>
2020-01-17 22:03:41 -0500

    change 3

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
 
+var textbox = document.getElementById("textbox");
+var pwdBtn = document.getElementById("pwdbtn");
+var copyBtn = document.getElementById("copybtn");
+var specials = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numeric = "1234567890";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charOptions = "";
+var convLength = 0;
 
-
-        var specials= "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-        var numeric ="1234567890";
-        var lowercase ="abcdefghijklmnopqrstuvwxyz";
-        var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var charOptions ="";
-
-    
-        var lower =confirm(" Do you need Lowercase letters?");
-        var upper =confirm(" Do you need Uppercase letters?");
-        var symbols =confirm(" Do you need specials Characters?");
-        var number =confirm(" Do you need Numbers?");
-        var length = prompt("length of your password?");
-        var convLength = parseInt(length);
-    
-        if (lower){
-            charOptions+=lowercase
-        }
-        if(upper){
-            charOptions+=uppercase
-        }
-        if(symbols){
-            charOptions+=specials
-        }
-        if(number){
-            charOptions+=numeric
-        }
-    
-      
-
-console.log(password(convLength,charOptions));
-
-function password (length,charOptions){
-    var pwd='';
-
-    for(var i=0;i<length;i++){
-        pwd+= charOptions.charAt(Math.floor(Math.random()*charOptions.length))
-        
-    }
-    return pwd;
-    
+function start() {
+  var lower = confirm("Do you need Lowercase letters?");
+  var upper = confirm("Do you need Uppercase letters?");
+  var symbols = confirm("Do you need specials Characters?");
+  var number = confirm("Do you need Numbers?");
+  var length = prompt("length of your password?");
+  convLength = parseInt(length);
+  if(lower){
+    charOptions += lowercase;
+  }
+  if(upper){
+    charOptions += uppercase;
+  }
+  if(symbols){
+    charOptions += specials;
+  }
+  if(number){
+    charOptions += numeric;
+  }
 }
 
-var textBox = document.getElementById("textbox");
-var pwdBtn = document.getElementById("pwdbtn");
+window.addEventListener("load",function() {
+    start();
+});
 
-
- pwdBtn.addEventListener("click",function(){
-     event.preventDefault();
- 
+function generatePassword() {
   
-textBox.value = (password(convLength,charOptions));
+  var pwd = '';
+  for(var i = 0; i < convLength; i++){
+    pwd += charOptions.charAt(Math.floor(Math.random() * charOptions.length));
+  }
+  return pwd;
+}
 
+pwdBtn.addEventListener("click", function() {
+  event.preventDefault();
+  textbox.value = generatePassword();
+});
 
- });
+copyBtn.addEventListener("click",function(){
+    textbox.select();
 
-
+    document.execCommand("copy");
+});
